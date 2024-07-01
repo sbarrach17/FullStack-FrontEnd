ba9be8aadfb1d203e72d36dd660504666d583d5d
Sebastián Barra <s.andresbarra@gmail.com>
2024-06-30 21:16:23 -0400

    url

--- src/App.jsx
+++ src/App.jsx
@@ -13,30 +13,56 @@ import CollectionDetails from "./views/CollectionDetails";
 import Cart from "./views/Cart";
 import Favorites from "./views/Favorites";
 import EditProduct from "./components/EditProduct";
-
+import { use } from "../../server/routes/user.Routes";
+import { useEffect } from "react";
 
 const App = () => {
-  return (
-    <>
-      <Navigation />
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/registrarse" element={<Registro />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/perfil" element={<Perfil />} />
-        <Route path="/collection" element={<Collection />} />
-        <Route path="/collection/:id" element={<CollectionDetails />} />
-        <Route path="/editarPerfil" element={<EditProfile />} />
-        <Route path="/agregarProducto" element={<AddProduct />} />
-        <Route path="/editarProducto/:productId" element={<EditProduct />} />
-        <Route path="/publicaciones" element={<Publications />} />
-        <Route path="/carro" element={<Cart />} />
-        <Route path="/favoritos" element={<Favorites />} />
+  useEffect(() => {
+    document.title = 'Product Form';
 
-      </Routes>
-    </>
-  );
+    const getDateServer = async () => {
+      try {
+        const response = await fetch(
+          "https://fullstack-backend-dult.onrender.com"
+        );
+        if (response.ok) {
+          const data = await response.text();
+          console.log("RESPUESTA DEL ENDPOINT", data);
+        } else {
+          console.error("Fallo");
+        }
+      } catch (error) {
+        console.error("Fallo", error);
+      }
+    };
+
+    getDateServer();
+  }, []);
+
+    return (
+        <>
+            <Navigation />
+
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/registrarse" element={<Registro />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/perfil" element={<Perfil />} />
+                <Route path="/collection" element={<Collection />} />
+                <Route path="/collection/:id" element={<CollectionDetails />} />
+                <Route path="/editarPerfil" element={<EditProfile />} />
+                <Route path="/agregarProducto" element={<AddProduct />} />
+                <Route
+                    path="/editarProducto/:productId"
+                    element={<EditProduct />}
+                />
+                <Route path="/publicaciones" element={<Publications />} />
+                <Route path="/carro" element={<Cart />} />
+                <Route path="/favoritos" element={<Favorites />} />
+            </Routes>
+        </>
+    );
 };
 
 export default App;
